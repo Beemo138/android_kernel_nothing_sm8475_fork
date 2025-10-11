25dd49df2d7b0e577b9f25d4402b7eeb56b591c3
Kareem Farid <kareefardi@users.noreply.github.com>
2025-10-11 18:30:23 +0000

    update patches

--- patch_kernel.sh
+++ patch_kernel.sh
@@ -1,6 +1,8 @@
+set -x
 patch --forward -p1 --forward < kernel_patches/next/next_hooks.patch
 patch --forward -p1 --forward < susfs4ksu/kernel_patches/50_add_susfs_in_gki-android13-5.10.patch
 patch -d KernelSU-Next -p1 --forward < susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch
+patch -d . -p1 --forward < susfs4ksu/kernel_patches/50_add_susfs_in_gki-android13-5.10.patch
 
 patch -d KernelSU-Next -p0 --forward < kernel_patches/next/susfs_fix_patches/v1.5.9/fix_apk_sign.c.patch
 patch -d KernelSU-Next -p0 --forward < kernel_patches/next/susfs_fix_patches/v1.5.9/fix_core_hook.c.patch
