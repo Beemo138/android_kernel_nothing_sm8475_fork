d6f895816064c05915dea159c9e21a8d88750cfb
Kareem Farid <kareefardi@users.noreply.github.com>
2025-10-12 15:50:10 +0000

    suski ultra

--- patch_kernel.sh
+++ patch_kernel.sh
@@ -1,11 +1,10 @@
 set -x
-patch --forward -p1 --forward < kernel_patches/next/next_hooks.patch
+# patch --forward -p1 --forward < kernel_patches/next/next_hooks.patch
+patch -d KernelSU -p1 --forward < susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch
 patch --forward -p1 --forward < susfs4ksu/kernel_patches/50_add_susfs_in_gki-android13-5.10.patch
-patch -d KernelSU-Next -p1 --forward < susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch
-patch -d . -p1 --forward < susfs4ksu/kernel_patches/50_add_susfs_in_gki-android13-5.10.patch
 
-patch -d KernelSU-Next -p0 --forward < kernel_patches/next/susfs_fix_patches/v1.5.9/fix_apk_sign.c.patch
-patch -d KernelSU-Next -p0 --forward < kernel_patches/next/susfs_fix_patches/v1.5.9/fix_core_hook.c.patch
-patch -d KernelSU-Next -p0 --forward < kernel_patches/next/susfs_fix_patches/v1.5.9/fix_kernel_compat.c.patch
-patch -d KernelSU-Next -p1 --forward < kernel_patches/next/susfs_fix_patches/v1.5.9/fix_sucompat.c.patch
+patch -d KernelSU -p0 --forward < kernel_patches/wild/susfs_fix_patches/v1.5.9/fix_apk_sign.c.patch
+patch -d KernelSU -p0 --forward < kernel_patches/wild/susfs_fix_patches/v1.5.9/fix_core_hook.c.patch
+patch -d KernelSU -p0 --forward < kernel_patches/wild/susfs_fix_patches/v1.5.9/fix_kernel_compat.c.patch
+patch -d KernelSU -p1 --forward < kernel_patches/wild/susfs_fix_patches/v1.5.9/fix_sucompat.c.patch
 
